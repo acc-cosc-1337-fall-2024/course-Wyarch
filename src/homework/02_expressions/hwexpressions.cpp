35010d6cc0cc13a615afb8388d1028dc4c856fb6
Wyarch <ian.wyatt@g.austincc.edu>
2024-09-14 02:18:33 +0000

    HW2 COMPLETED

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -7,7 +7,18 @@ int add_numbers(int num1, int num2)
 }
 
 //write function code here
+double get_sales_tax_amount(double meal_amount)
+{
+    double tax_rate = 0.0675;
+    return meal_amount * tax_rate;
+}
 
+double get_tip_amount(double meal_amount, double tip_rate)
+{
+    //I divided the tip rate by 100 for correct math while also
+    // making it simpler for user interfacing with percentages.
+    return meal_amount * (tip_rate/100);
+}
 
 
 
--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,11 +1,38 @@
 ﻿//write include statements
+#include <iostream>
+#include "hwexpressions.h"
 
 
 //write namespace using statement for cout
+using std::cout; using std::cin;
 
 int main()
 {
+	//Variable Declaration
+	double meal_amount;
+	double tip_rate;
+	double tip_amount;
+	double tax_amount;
+	double total;
+
+	//Begin Processing
+	cout<<"Enter the meal total in dollars: ";
+	cin>>meal_amount;
+	cout<<"\n Enter the tip rate in percentage (%): ";
+	cin>>tip_rate;
 	
+	tax_amount = get_sales_tax_amount(meal_amount);
+	tip_amount = get_tip_amount(meal_amount,tip_rate);
+
+	//Add all values into total
+	total = tax_amount + tip_amount + meal_amount;
+
+	//Display receipt
+	cout<<"Meal Amount: " <<meal_amount <<"\n";
+	cout<<"Sales Tax:   " <<tax_amount <<"\n";
+	cout<<"Tip Amount:  " << tip_amount <<"\n";
+	cout<<"Total:       " <<total <<"\n";
+
 
 	return 0;
 }
