24c35c496a3a53d5bc6492dcbf3e61a6734101e2
Wyarch <ian.wyatt@g.austincc.edu>
2024-09-23 23:53:43 +0000

    04 Examples

--- src/examples/04_repetition/02_for/for.cpp
+++ src/examples/04_repetition/02_for/for.cpp
@@ -1,6 +1,27 @@
 #include "for.h"
 #include<iostream>
 
+using std::cout;
+
+void display_hello_for(int num)
+{
+    for(auto i = 0; i < num; i++)
+    {
+        cout<<"Hello\n";
+    }
+}
+
+int sum_of_squares_for(int num)
+{
+    auto total = 0;
+
+    for(auto i = 0; i <= num; i++)
+    {
+        total += i * i;
+    }
+
+    return total;
+}
 //Write code for function display_numbers that accepts a number and 
 //displays every number that precedes(starting from 1) it in a line
 //Example: display_numbers(5)
