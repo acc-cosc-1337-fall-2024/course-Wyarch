d473eb4439eb49f3378c619bbfc050c0e504e040
Wyarch <ian.wyatt@g.austincc.edu>
2024-09-26 22:26:46 +0000

    HW4 COMPLETED FINAL DRAFT WITH MAIN

--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -25,6 +25,7 @@ int main()
 				cin>>num1;
 				cout<<"The factorial to your number is "<<factorial(num1)<<"\n\n";
 				break;
+			
 			case 2:
 				cout<<"Greatest Common Divisor selected...\nPlease enter your first integer: ";
 				cin>>num1;
@@ -32,10 +33,31 @@ int main()
 				cin>>num2;
 				cout<<"Your Greatest Common Divisor is "<<gcd(num1,num2)<<"\n\n";
 				break;
+			
 			case 3:
-				cout<<"Exiting program...\n";
+				do
+				{
+					cout<<"Are you sure you want to exit?\n1. Yes\n2. No\nPlease select an option using an integer: ";
+					cin>>userSelection;
+
+					if(userSelection == 1)
+					{
+						cout<<"Exiting program...\n";
+					}
+					else if(userSelection == 2)
+					{
+						cout<<"Returning to program...\n\n";
+					}
+					else
+					{
+						cout<<"Invalid input. Please enter an integer from 1-2.\n";
+					}
+				} while(userSelection != 1 && userSelection != 2);
+				userSelection = userSelection < 2 ? 3:1;
 				break;
+
 			default:
+				cout<<"Invalid input. Please enter an integer from 1 - 3.\n";
 				break;
 		}
 	} while(userSelection != 3);
