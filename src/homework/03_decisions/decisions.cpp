e7b3287c63e21177efd56d040ee4982fe12d777e
Wyarch <ian.wyatt@g.austincc.edu>
2024-10-23 00:01:08 +0000

    HW3 UPDATED AND COMPLETE

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,57 @@
 //write include statement for decisions header
-
+#include "decisions.h"
 
 //Write code for function(s) code here
+string get_letter_grade_using_if(int grade)
+{
+    string letterGrade;
+
+    if(grade >= 90 && grade <= 100)
+    {
+        letterGrade = "A";
+    }
+    else if(grade >= 80 && grade <= 89)
+    {
+        letterGrade = "B";
+    }
+    else if(grade >= 70 && grade <= 79)
+    {
+        letterGrade = "C";
+    }
+    else if(grade >= 60 && grade <= 69)
+    {
+        letterGrade = "D";
+    }
+    else if(grade >= 0 && grade <= 59)
+    {
+        letterGrade = "F";
+    }
+
+    return letterGrade;
+}
+
+string get_letter_grade_using_switch(int grade)
+{
+    string letterGrade;
+
+    switch(grade)
+    {
+        case 1 ... 59:
+            letterGrade = "F";
+            break;
+        case 60 ... 69:
+            letterGrade = "D";
+            break;
+        case 70 ... 79:
+            letterGrade = "C";
+            break;
+        case 80 ... 89:
+            letterGrade = "B";
+            break;
+        case 90 ... 100:
+            letterGrade = "A";
+            break;
+    }
+
+    return letterGrade;
+}
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,42 @@
 //write include statements
+#include "decisions.h"
 
 int main() 
 {
+	//initialize variables
+	int menuSelection = 0;
+	bool exitFlag = false;
+	int grade;
+	//initialize do-while loop
+	do
+	{
+		//Display menu and retrieve user input
+		cout<<"Menu\n1. Get letter grade using if\n2. Get letter grade using switch\n3. Exit\n";
+		cin>>menuSelection;
+		//initilize if-else-if statement
+		if(menuSelection == 1)
+		{
+			cout<<"Get letter grade using if selected...\nPlease enter the grade: ";
+			cin>>grade;
+			cout<<"The letter grade is: "<<get_letter_grade_using_if(grade)<<"\n\n";
+		}
+		else if(menuSelection == 2)
+		{
+			cout<<"Get letter grade using switch selected...\nPlease enter the grade: ";
+			cin>>grade;
+			cout<<"The letter grade is: "<<get_letter_grade_using_switch(grade)<<"\n\n";
+		}
+		else if(menuSelection == 3)
+		{
+			cout<<"Exiting Program...\n";
+			exitFlag = true;
+		}
+		else
+		{
+			cout<<"Invalid selection. Please enter a value from 1 - 3 to make a selection.\n\n";
+		}
+
+	} while(exitFlag != true);
+	
 	return 0;
 }
