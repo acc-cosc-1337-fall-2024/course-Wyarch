8de35f8b07815c01fb3424e94533e6a16f674dd0
Wyarch <ian.wyatt@g.austincc.edu>
2024-11-17 01:34:08 +0000

    TicTacToe4 Functions Complete

--- src/homework/06_tic_tac_toe/tic_tac_toe_4.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe_4.cpp
@@ -11,7 +11,22 @@ else
 false
 */
 
-
+bool TicTacToe::check_column_win()
+{
+    for(int i = 0; i < 4; i+=1)
+    {
+        if(spots[i] == "X" && spots[i+4] == "X" && spots[i+8] == "X" && spots[i+12] == "X")
+        {
+            return true;
+        }
+        else if(spots[i] == "O" && spots[i+4] == "O" && spots[i+8] == "O" && spots[i+12] == "O")
+        {
+            return true;
+        }
+    }
+    
+    return false;
+}
 
 
 /*
@@ -23,7 +38,22 @@ Win by row if
 12,13,14, 15 are equal
 */
 
+bool TicTacToe::check_row_win()
+{
+    for(int i = 0; i < 16; i+=4)
+    {
+        if(spots[i] == "X" && spots[i+1] == "X" && spots[i+2] == "X" && spots[i+3] == "X")
+        {
+            return true;
+        }
+        else if(spots[i] == "O" && spots[i+1] == "O" && spots[i+2] == "O" && spots[i+3] == "O")
+        {
+            return true;
+        }
+    }
 
+    return false;
+}
 
 /*
 class function check_diagonal_win
@@ -34,3 +64,78 @@ Win diagonally
 12,13,14, 15
 
 */
+
+bool TicTacToe::check_diagonal_win()
+{
+    int count;
+
+    for(int i = 0; i < 2; i++)
+    {
+        count = 0;
+        if(i == 0)
+        {
+            for(int c = 0; c < spots.size(); c+=5)
+            {
+                if(spots[c] == "X")
+                {
+                    count++;
+                }
+            }
+        }
+        else
+        {
+            for(int c = 2; c < spots.size(); c+=3)
+            {
+                if(spots[c] == "X")
+                {
+                    count++;
+                }
+            }
+        }
+
+        if(count == 4)
+        {
+            return true;
+        }
+    }
+
+    for(int i = 0; i < 2; i++)
+    {
+        count = 0;
+        if(i == 0)
+        {
+            for(int c = 0; c < spots.size(); c+=5)
+            {
+                if(spots[c] == "O")
+                {
+                    count++;
+                }
+            }
+        }
+        else
+        {
+            for(int c = 2; c < spots.size(); c+=3)
+            {
+                if(spots[c] == "O")
+                {
+                    count++;
+                }
+            }
+        }
+
+        if(count == 4)
+        {
+            return true;
+        }
+    }
+
+    return false;
+
+    /*for(int i = 0; i < spots.size(); i+4)
+    {
+        if(spots[i] == "O")
+        {
+            countO++;
+        }
+    }*/
+}
--- src/homework/06_tic_tac_toe/tic_tac_toe_4.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe_4.h
@@ -1,1 +1,21 @@
 //h
+#include "tic_tac_toe.h"
+
+#ifndef TIC_TAC_TOE_4
+#define TIC_TAC_TOE_4
+
+class TicTacToe4:TicTacToe
+{
+public:
+    
+    TicTacToe4():TicTacToe(4){};
+
+private:
+
+    bool check_diagonal_win();
+    bool check_row_win();
+    bool check_column_win();
+
+};
+
+#endif
